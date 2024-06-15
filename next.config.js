d4697201843174c55a401d3e46cb8d51762fa254
cym1212 <william5680@naver.com>
2024-06-16 02:25:15 +0900

    구글 Search Console 적용

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID,
+    NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
+  },
 }
